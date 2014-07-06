02dcc99658d3638232622ec74b2aaa1e12eeceb9
Michael Aufreiter <michael@substance.io>
2014-07-06 14:30:53 +0200

    Smarter Compiler.
    Previewing should work much faster now, e.g. when videos are used.

--- compiler.js
+++ compiler.js
@@ -1,10 +1,12 @@
 "use strict";
 
 var _ = require("underscore");
-var JSZip = require("jszip");
 var fs = require("fs");
 var util = require("substance-util");
 var Fragmenter = util.Fragmenter;
+
+var Article = require("substance-article");
+
 
 var Renderer = function(doc, templatePath, templateParams) {
   this.doc = doc;
@@ -155,50 +157,90 @@ var Compiler = function(doc) {
 
 Compiler.Prototype = function() {
 
-  this.compile = function(templatePath, templateParams, cb) {
-    var result = new JSZip();
+
+  // Compile publication
+  // --------
+  //
+  // templatePath specifies the template being used
+  // targetPath is the target directory
+  // templateParams contains user settings, e.g. if a table of contents should be used
+
+  this.compile = function(templatePath, targetPath, templateParams, cb) {
     var doc = this.doc;
+
+    var oldFiles = {};
+
+    // Delete old files if there are any
+    if (fs.existsSync(targetPath+"/content.json")) {
+      var oldDocContent = JSON.parse(fs.readFileSync(targetPath+"/content.json", {encoding: "utf-8"}));
+      var oldDoc = Article.fromSnapshot(oldDocContent);
+      var fileIndex = oldDoc.getIndex("files");
+      
+      _.each(fileIndex.nodes, function(fileId) {
+        var file = oldDoc.get(fileId);
+        oldFiles[fileId] = file;
+      });
+    }
 
     // Render the page
     var indexHTML = new Renderer(this.doc, templatePath, templateParams).render();
 
-    result.file("index.html", indexHTML);
+    // Write index.html
+    fs.writeFileSync(targetPath + "/index.html", indexHTML, "utf8");
+
     var assetsDir = templatePath+"/assets";
 
     // Copy assets to destination dir
-    var assets = fs.readdirSync(templatePath+"/assets");
-
+    var assets = fs.readdirSync(assetsDir);
     _.each(assets, function(fileName) {
-      var sourcePath = assetsDir + "/"+fileName;
-      var fileBuffer = fs.readFileSync(sourcePath);
-      result.file(fileName, fileBuffer);
+      var sourceFile = assetsDir + "/"+fileName;
+      var targetFile = targetPath + "/"+fileName;
+      fs.createReadStream(sourceFile).pipe(fs.createWriteStream(targetFile));
     });
+
+
+    function writeFile(file) {
+      if (file.isBinary()) {
+        var buffer = new Buffer(new Uint8Array(file.getData()));
+        fs.writeFileSync(targetPath + "/"+file.id, buffer);
+      } else if (!file.isJSON()) {
+        // For some reason, writing publish.json makes troubles,
+        // so we just write non-json files here
+        fs.writeFileSync(targetPath + "/"+fileName, file.getData(), "utf8");
+      }
+    }
 
     // Also write binary files
     var fileIndex = doc.getIndex("files");
     _.each(fileIndex.nodes, function(fileId) {
       var file = doc.get(fileId);
-      if (file.isBinary()) {
-        result.file(fileId, file.getData());
-      } else if (!file.isJSON()) {
-        // For some reason, writing publish.json makes troubles
-        result.file(fileId, file.getData());
+      var oldFile = oldFiles[fileId];
+
+      // File is already there, should we be smart?
+      if (oldFile) {
+        if (file.version !== oldFile.version) {
+          // overwrite
+          writeFile(file);
+        } // else just keep the existing file
+
+        delete oldFiles[fileId];
+      } else {
+        writeFile(file);
       }
     });
 
-    if (templateParams.include_source_file) {
-      // Attach source file
-      util.zip.zip(doc, function(err, zip) {
-        var data = zip.generate({type: "nodebuffer"});
-        result.file(util.slug(doc.title)+".sdf", data);
-        result.file("content.json", JSON.stringify(doc.toJSON(), null, "  "));
+    // Remove old files that are no longer referenced to keep the output folder clean
+    _.each(oldFiles, function(oldFile, fileId) {
+      if (fs.existsSync(targetPath+"/"+oldFile.id)) {
+        // console.log('removing..', targetPath+"/"+oldFile.id);
+        fs.unlinkSync(targetPath+"/"+oldFile.id);
+      }
+    });
 
-        cb(null, result);
-      });
-    } else {
-      cb(null, result);  
-    }    
-    
+    // Write content.json
+    fs.writeFileSync(targetPath + "/content.json", JSON.stringify(doc, null, "  "), "utf8");
+
+    cb(null);
   };
 };
 
